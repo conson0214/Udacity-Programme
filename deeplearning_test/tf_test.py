10bebb51f4c8f02464e374ada9bd52e51603746d
conson0214 <conson0214@163.com>
2017-12-01 16:28:21 +0800

    update deeplearning unit materials

---
+++ deeplearning_test/tf_test.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import tensorflow as tf
+
+# Create TensorFlow object called tensor
+hello_constant = tf.constant('Hello World!')
+
+with tf.Session() as sess:
+    # Run the tf.constant operation in the session
+    output = sess.run(hello_constant)
+    print(output)
